900dea1654940a3fe324b4df612db6565459925e
juliokhichfy <juliocesark@gmail.com>
2018-10-22 11:08:23 -0300

    fazendo edição, delete, inserção

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,28 +1,18 @@
-/*module.exports.associacoes = function(application, req, res){
-
-	var connection = application.config.dbConnection();
-	var dadosModel = new application.app.models.DadosDAO(connection);
-	dadosModel.getAssociacoes(function(error, result){
-		res.render("home/associacoes", {associacoes : result});
-		//res.send()
-	});
-
-}*/
 
 module.exports.filtrar = function(application, req, res){
 	var connection = application.config.dbConnection();
 	var dadosModel = new application.app.models.DadosDAO(connection);
+	var tipo = req.body['tipos'];
 	var tipos;
-	console.log("tipo a ser filtrado " + req.body['tipos']);
 	dadosModel.getTipos(function(error,result){
 		tipos=result;
 	});
-
-	dadosModel.getAssociacoes(req.body['tipos'], function(error, result){
+	var tbnaux = tipo.replace(' ', '');
+	var tbn = tbnaux.replace('-', '');
+	dadosModel.getTable(tbn, tipo, function(error, result){
 		res.render("home/lista", {listagem : result, tipos:tipos});
 		//res.send(result);
 	});
-
 }
 
 module.exports.show = function(application, req, res){
@@ -33,18 +23,19 @@ module.exports.show = function(application, req, res){
 		res.render("home/lista", {listagem:listagem, tipos:result});
 		//res.send(result);
 	});
+
 	
 }
 
 module.exports.editar = function(application, req, res){
-	console.log("EDITANDO....");
 	var connection = application.config.dbConnection();
 	var dadosModel = new application.app.models.DadosDAO(connection);
 
-	var id = req.query;
-
-	dadosModel.getAssociacao(id, function(error, result){
-		//res.render("home/index", {associacao : result});
-		res.send(result);
+	var id = req.query['id'];
+	var tablename = req.query['tablename'];
+	
+	dadosModel.getTupla(id, tablename,function(error, result){
+		res.render("home/editar", {tupla : result, tablename:tablename});
+		//res.send(result);
 	});	
 }
--- app/models/DadosDAO.js
+++ app/models/DadosDAO.js
@@ -2,19 +2,20 @@ function DadosDAO(connection){
 	this._connection = connection;
 }
 
-DadosDAO.prototype.getAssociacoes = function(tipos, callback){
-	console.log(">>>> tipos " + tipos);
-	var tableName = tipos.replace(' ', '');
-	console.log(">>>> tableName " + tableName);
-	this._connection.query("select * from "+tableName+" where tipo = ?", tipos, callback);
+DadosDAO.prototype.getTable = function(tablename,tipo, callback){
+	this._connection.query("select * from "+tablename+" where tipo = ?", tipo, callback);
 }
 
-DadosDAO.prototype.getAssociacao = function(id, callback){
-	this._connection.query('select * from ASSOCIACOES where id= ' + id.id, callback);
+DadosDAO.prototype.getTupla = function(id, tablename, callback){
+	var table = tablename.replace(" ", "");
+	table = table.replace("-", "");
+	console.log("no model "+ id, " " + table);
+	this._connection.query("select id, tratamento, nome, cargo, email, telefone, cep, endereco, sexo, situacao from "+ table +" where id = " + id, callback);
 }
+
 
 DadosDAO.prototype.getTipos = function(callback){
-	this._connection.query('select distinct tipo from associacoes union select distinct tipo from autoridadesespeciais', callback);
+	this._connection.query('SELECT distinct tipo FROM ASSOCIACOES UNION SELECT  distinct tipo FROM AUTORIDADESESPECIAIS', callback);
 }
 
 /*NoticiasDAO.prototype.getNoticia = function(id_noticia, callback){
